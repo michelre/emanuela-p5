7b90b7fc5ec9c4b790573e045cfa26c5d01a2318
Emanuela Palazzo Lacanfora <62099675+emanuelapalazzolacanfora@users.noreply.github.com>
2020-07-19 01:22:10 +0200

    Update ficheproduit.js

--- js/ficheproduit.js
+++ js/ficheproduit.js
@@ -64,3 +64,18 @@ async function detailProduit(){
 		}
 	}
 
+	 /*Fonction ajouter le produit au panier de l'utilisateur
+ **********************************************/
+addToCart = () =>{
+	//Au clic de l'user pour mettre le produit dans le panier
+	let inputBuy = document.getElementById("ajoutduproduitaupanier");
+	inputBuy.addEventListener("click", async function() {
+		const produits = await get();
+	//Récupération du panier dans le localStorage et ajout du produit dans le panier avant revoit dans le localStorage
+	userPanier.push(produits);
+	localStorage.setItem("userPanier", JSON.stringify(userPanier));
+	console.log("Administration : le produit a été ajouté au panier");
+	alert("Vous avez ajouté ce produit dans votre panier")
+});
+};
+
