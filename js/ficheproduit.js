043d931c59b8762f313f298918674c2fd71e6c04
Emanuela Palazzo Lacanfora <62099675+emanuelapalazzolacanfora@users.noreply.github.com>
2020-07-19 04:18:51 +0200

    Update ficheproduit.js

--- js/ficheproduit.js
+++ js/ficheproduit.js
@@ -4,29 +4,56 @@
 const produitSell = "cameras"  //Au choix entre : "cameras" - "furniture" - "teddies"
 const apiUrl = "https://oc-p5-api.herokuapp.com/api/" + produitSell + "/";
 
-//id du produit pour permettre un tri dans l'API
+//id des différents produits dans l'API
 
 let idProduit = "";
 
+/*Appel de l'API
+**********************************************/
 
+get = () =>{
+	return new Promise((resolve) =>{
+		let request = new XMLHttpRequest();
+		request.onreadystatechange = function() {
+			if(this.readyState == XMLHttpRequest.DONE && this.status == 200) 
+			{
+				resolve(JSON.parse(this.responseText));
+				console.log("Vous êtes connecté à l'API");
 
-get (apiUrl).then((response)=>{
-
-	async function detailProduit(){
-		//Collecter l'URL après le ?id= pour le récupérer uniquement sur l'API
-		idProduit = location.search.substring(4);
-		const produitSelected = await get();
-		
-		//élément de l'API a insérer dans le document HTML
-		document.getElementById("imageduproduit").setAttribute("src", produitSelected.imageUrl);
-		document.getElementById("nomduproduit").innerHTML = produitSelected.name;
-		document.getElementById("descriptionduproduit").innerHTML = produitSelected.description;
-		document.getElementById("prixduproduit").innerHTML = produitSelected.price / 100 + " €";
-	
-		
-		produitSelected.lenses.forEach((produit)=>{
-				let produitOption = document.createElement("option");
-				document.getElementById("option").appendChild(produitOption).innerHTML = produit;
-			});
-		};
+				//L'appel est réussi => suppression des message d'erreur
+				error = document.getElementById("erreur");
+				//On supprime le message d'erreur s'il existe
+				if(error){
+					error.remove();
+				}
+			}else{
+				console.log("Erreur de connexion à l'API");
+			}
+		}
+		request.open("GET", apiUrl + idProduit);
+		request.send();
 	});
+};
+
+/*Page HTML de la fiche produit sélectionnée
+**********************************************/
+
+async function detailProduit(){
+    //Collecter l'URL après le ?id= pour le récupérer uniquement sur l'API
+    idProduit = location.search.substring(4);
+    const produitSelected = await get();
+    
+    //élément de l'API a insérer dans le document HTML
+    document.getElementById("imageduproduit").setAttribute("src", produitSelected.imageUrl);
+    document.getElementById("nomduproduit").innerHTML = produitSelected.name;
+    document.getElementById("descriptionduproduit").innerHTML = produitSelected.description;
+    document.getElementById("prixduproduit").innerHTML = produitSelected.price / 100 + " €";
+
+    
+    produitSelected.lenses.forEach((produit)=>{
+    		let produitOption = document.createElement("option");
+    		document.getElementById("option").appendChild(produitOption).innerHTML = produit;
+    	});
+	};
+	
+
