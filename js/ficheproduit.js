b97a2bec0a497e8238862997bddeb5f4ed3ef645
Emanuela Palazzo Lacanfora <62099675+emanuelapalazzolacanfora@users.noreply.github.com>
2020-07-19 18:13:30 +0200

    Add files via upload

--- js/ficheproduit.js
+++ js/ficheproduit.js
@@ -8,52 +8,64 @@ const apiUrl = "https://oc-p5-api.herokuapp.com/api/" + produitSell + "/";
 
 let idProduit = "";
 
-/*Appel de l'API
-**********************************************/
-
-get = () =>{
-	return new Promise((resolve) =>{
-		let request = new XMLHttpRequest();
-		request.onreadystatechange = function() {
-			if(this.readyState == XMLHttpRequest.DONE && this.status == 200) 
-			{
-				resolve(JSON.parse(this.responseText));
-				console.log("Vous êtes connecté à l'API");
-
-				//L'appel est réussi => suppression des message d'erreur
-				error = document.getElementById("erreur");
-				//On supprime le message d'erreur s'il existe
-				if(error){
-					error.remove();
-				}
-			}else{
-				console.log("Erreur de connexion à l'API");
-			}
-		}
-		request.open("GET", apiUrl + idProduit);
-		request.send();
-	});
-};
+//Collecter l'URL après le ?id= pour le récupérer uniquement sur l'API
+idProduit = location.search.substring(4);
 
 /*Page HTML de la fiche produit sélectionnée
 **********************************************/
 
-async function detailProduit(){
-    //Collecter l'URL après le ?id= pour le récupérer uniquement sur l'API
-    idProduit = location.search.substring(4);
-    const produitSelected = await get();
+get (apiUrl+idProduit).then((response)=>{
+    
     
     //élément de l'API a insérer dans le document HTML
-    document.getElementById("imageduproduit").setAttribute("src", produitSelected.imageUrl);
-    document.getElementById("nomduproduit").innerHTML = produitSelected.name;
-    document.getElementById("descriptionduproduit").innerHTML = produitSelected.description;
-    document.getElementById("prixduproduit").innerHTML = produitSelected.price / 100 + " €";
+    let produitImage = document.getElementById("imageduproduit")
+    produitImage.setAttribute("src", response.imageUrl);
+    let produitNom = document.getElementById("nomduproduit")
+    produitNom.innerHTML = response.name;
+    let produitDescription = document.getElementById("descriptionduproduit")
+    produitDescription.innerHTML = response.description;
+    let produitPrix = document.getElementById("prixduproduit")
+    produitPrix.innerHTML = response.price / 100 + " €";
 
-    
-    produitSelected.lenses.forEach((produit)=>{
-    		let produitOption = document.createElement("option");
-    		document.getElementById("option").appendChild(produitOption).innerHTML = produit;
+    //Selon le type de produit (ligne 3) création des options
+    switch(produitSell){
+    	case "cameras":
+            response.lenses.forEach((produit)=>{
+    		let optionProduit = document.createElement("option");
+    		document.getElementById("option").appendChild(optionProduit).innerHTML = produit;
     	});
-	};
+    	break;
+    	case "furniture":
+            response.varnish.forEach((produit)=>{
+    		let optionProduit = document.createElement("option");
+    		document.getElementById("option").appendChild(optionProduit).innerHTML = produit;
+    	});
+    	break;
+    	case "teddies":
+            response.colors.forEach((produit)=>{
+    		let optionProduit = document.createElement("option");
+    		document.getElementById("option").appendChild(optionProduit).innerHTML = produit;
+    	});
+    	break;
+    	default:
+    	console.log("Administration : Veuillez bien renseigner la variable produitSell ligne 4 du fichier ficheproduit.js");
+    }
+
+      
+    /*Fonction ajouter le produit au panier de l'utilisateur
+ **********************************************/
+ addToCart= () =>{
+    //Au clic de l'user pour mettre le produit dans le panier
+    let AddProductToCart = document.getElementById("ajoutduproduitaupanier");
+    AddProductToCart.addEventListener("click",() =>{
+    
+    //Récupération du panier dans le localStorage et ajout du produit dans le panier avant revoit dans le localStorage
+    userPanier.push(response);
+    localStorage.setItem("userPanier", JSON.stringify(userPanier));
+    console.log("Administration : le produit a été ajouté au panier");
+    alert("Vous avez ajouté ce produit dans votre panier")
+});
+};
+	});
 	
 
