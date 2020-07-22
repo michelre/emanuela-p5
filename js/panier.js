02d478f7b5ff1b3a4205289d6db589a65f2e3b3a
Emanuela Palazzo Lacanfora <62099675+emanuelapalazzolacanfora@users.noreply.github.com>
2020-07-22 14:18:06 +0200

    Update panier.js

--- js/panier.js
+++ js/panier.js
@@ -33,6 +33,7 @@ const renderCart = (productName, productPrice, imgUrl, productQuantity) => {
     cart.appendChild(article)
 };
 /* Supprime élément du panier sur un clique*/
+const removeElt = document.querySelector(".supprime"); 
 const deleteCart = (removeElt, container, productId) => {
     removeElt.addEventListener("click", () => {
         const panier = JSON.parse(localStorage.getItem("panier"));
@@ -50,6 +51,7 @@ const deleteCart = (removeElt, container, productId) => {
 
 // soustrait et enlève un produit au panier
 
+const IconMoins = document.querySelector(".fas fa-minus-circle");
 const soustractionItem = (iconMoins, container, productId) => {
     iconMoins.addEventListener("click", () => {
         const panier = JSON.parse(localStorage.getItem("panier"));
@@ -69,6 +71,7 @@ const soustractionItem = (iconMoins, container, productId) => {
 
 // additionne et rajoute un produit au panier
 
+const IconPlus = document.querySelector(".fas fa-plus-circle");
 const additionItem = (iconPlus, container, productId) => {
     iconPlus.addEventListener("click", () => {
         const panier = JSON.parse(localStorage.getItem("panier"));
