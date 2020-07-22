04d6f120d3dda9974597832fc5014e94ce9cd7f5
Emanuela Palazzo Lacanfora <62099675+emanuelapalazzolacanfora@users.noreply.github.com>
2020-07-22 18:50:26 +0200

    Update panier.js

--- js/panier.js
+++ js/panier.js
@@ -34,7 +34,6 @@ const renderCart = (productName, productPrice, imgUrl, productQuantity) => {
     cart.appendChild(article)
 };
 /* Supprime élément du panier sur un clique*/
-let= removeElt.getElementByClassName("supprime")
 const deleteCart = (removeElt, container, productId) => {
     removeElt.addEventListener("click", () => {
         const panier = JSON.parse(localStorage.getItem("panier"));
