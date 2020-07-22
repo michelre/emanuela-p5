f4538ba8396805b8ae4d9746d58f35ff3dbf8467
Emanuela Palazzo Lacanfora <62099675+emanuelapalazzolacanfora@users.noreply.github.com>
2020-07-22 13:54:47 +0200

    Update panier.js

--- js/panier.js
+++ js/panier.js
@@ -4,125 +4,86 @@
 const produitSell = "cameras"  //Au choix entre : "cameras" - "furniture" - "teddies"
 const apiUrl = "https://oc-p5-api.herokuapp.com/api/" + produitSell + "/";
 
-//id des différents produits dans l'API
-
-let idProduit = "";
-
-//Collecter l'URL après le ?id= pour le récupérer uniquement sur l'API
-idProduit = location.search.substring(4);
-
-const cart = document.querySelector("#cart"); // Récupère la section du panier
-const cartTotal = document.getElementById("cart-total"); //Récupère le h3 pour le prix total
+const cart = document.querySelector("#panier"); // Récupère la section du panier
+const cartTotal = document.getElementById("panier-total"); //Récupère le h3 pour le prix total
 const form = document.querySelector("form"); // Récupère le formulaire
 
 const cartInformation = {
-  contact: {},
-  products: [],
+    contact: {},
+    products: [],
 };
 /* Stock le prix total */
 let totalPrice = 0;
 
-// Affiche le/les produit(s) du panier.
-get (apiUrl+idProduit).then((response)=>{
-  const cartItems = JSON.parse(localStorage.getItem("panier"));
-  if (Object.keys(cartItems).length > 0) {
-    for (let i = 0; i < Object.keys(cartItems).length; i++) {
-      // Pour chaque article du panier
-      const itemId = Object.keys(cartItems)[i];
-      const product = getItem(itemId); // Récupère les informations du produit
-      const camId = idProduit; // Stocke l'id du produit
-      const camName = response.name; // Stocke le nom du produit
-      const camPrice = response.price / 100; // Stocke le prix du produit
-      const camImg = response.imageUrl; // Stocke l'image du produit
-      const camQuantity = cartItems[itemId].quantity;
-      cartInformation.products.push(camId); // Envoie l'id du produit au tableau products de cartInformation
-      renderCart(camName, camPrice, camImg, camQuantity); // Fourni l'affichage du/des produits du panier
-
-      const remove = document.querySelectorAll(".supprime")[i];
-      const article = document.querySelectorAll("article")[i];
-      const iconMoins = document.querySelectorAll(".fas fa-minus-circle")[i];
-      const iconPlus = document.querySelectorAll("fas fa-plus-circle")[i];
-      iconMoins.style.fontSize = "20px";
-      iconPlus.style.fontSize = "20px";
-      deleteCart(remove, article, itemId);
-      soustractionItem(iconMoins, article, itemId); // soustraction
-      additionItem(iconPlus, article, itemId); // addition
-    }
-  } else {
-    cart.textContent = "Votre panier est vide.";
-    form.classList.add("invisible");
-  }
-});
-
 // Fourni l'affichage du/des produits du panier
 const renderCart = (productName, productPrice, imgUrl, productQuantity) => {
-  /* Affiche article(s) du panier */
-  const article = document.createElement("article");
-  article.innerHTML = `
-    <img src="${imgUrl}">
+    /* Affiche article(s) du panier */
+    const article = document.createElement("article");
+    article.innerHTML = `
+    <img src="${imgUrl}" width="50px">
     <div class="produit-information>
         <p class="produit-nom">${productName}</p>
         <p class="produit-prix">${productPrice}</p>
     </div>
     <p class="quantity"><i class="fas fa-minus-circle">${productQuantity}</i><i class="fas fa-plus-circle"></i></p>
     <p class="supprime ">supprimer</p>`;
-  cart.insertBefore(article, cartTotal); // Insère article avant cartTotal
-  totalPrice += productPrice * productQuantity; /* Implémente prix */
-  cartTotal.textContent = `Total : ${totalPrice}€`; /* Affiche le prix total */
-  console.log(article);
+    cart.insertBefore(article, cartTotal); // Insère article avant cartTotal
+    totalPrice += productPrice * productQuantity; /* Implémente prix */
+    cartTotal.textContent = `Total : ${totalPrice}€`; /* Affiche le prix total */
+    cart.appendChild(article)
 };
 /* Supprime élément du panier sur un clique*/
 const deleteCart = (removeElt, container, productId) => {
-  removeElt.addEventListener("click", () => {
-    const panier = JSON.parse(localStorage.getItem("panier"));
-    if (panier === null) return;
-    if (panier[productId] === undefined) return;
-    else {
-      delete panier[productId];
-    }
-    localStorage.setItem("panier", JSON.stringify(panier));
-    // ); /* Supprime item du localStorage */
-    container.remove(); /* Supprime item du DOM */
-    location.reload(true); /* Actualise la page dynamiquement */
-  });
+    removeElt.addEventListener("click", () => {
+        const panier = JSON.parse(localStorage.getItem("panier"));
+        if (panier === null) return;
+        if (panier[productId] === undefined) return;
+        else {
+            delete panier[productId];
+        }
+        localStorage.setItem("panier", JSON.stringify(panier));
+        // ); /* Supprime item du localStorage */
+        container.remove(); /* Supprime item du DOM */
+        location.reload(true); /* Actualise la page dynamiquement */
+    });
 };
 
 // soustrait et enlève un produit au panier
 
 const soustractionItem = (iconMoins, container, productId) => {
-  iconMoins.addEventListener("click",  () => {
-    const panier = JSON.parse(localStorage.getItem("panier"));
-    if (panier === null) return;
-    if (panier[productId] === undefined) return;
-    if (panier[productId].quantity > 1) {
-      panier[productId].quantity--;
-    } else {
-      delete panier[productId];
-    }
-    localStorage.setItem("panier", JSON.stringify(panier));
-    // ); /* Supprime item du localStorage */
-    container.remove(); /* Supprime item du DOM */
-    location.reload(true);
-  });
+    iconMoins.addEventListener("click", () => {
+        const panier = JSON.parse(localStorage.getItem("panier"));
+        if (panier === null) return;
+        if (panier[productId] === undefined) return;
+        if (panier[productId].quantity > 1) {
+            panier[productId].quantity--;
+        } else {
+            delete panier[productId];
+        }
+        localStorage.setItem("panier", JSON.stringify(panier));
+        // ); /* Supprime item du localStorage */
+        container.remove(); /* Supprime item du DOM */
+        location.reload(true);
+    });
 };
 
 // additionne et rajoute un produit au panier
 
 const additionItem = (iconPlus, container, productId) => {
-  iconPlus.addEventListener("click", () => {
-    const panier = JSON.parse(localStorage.getItem("panier"));
-    if (panier === null) return;
-    if (panier[productId] === undefined) return;
-    if (panier[productId].quantity >= 1) {
-      panier[productId].quantity++;
-    } else {
-      delete panier[productId];
-    }
-    localStorage.setItem("panier", JSON.stringify(panier));
-    // ); /* Supprime item du localStorage */
-    container.remove(); /* Supprime item du DOM */
-    location.reload(true);
-  });
+    iconPlus.addEventListener("click", () => {
+        const panier = JSON.parse(localStorage.getItem("panier"));
+        if (panier === null) return;
+        if (panier[productId] === undefined) return;
+        if (panier[productId].quantity >= 1) {
+            panier[productId].quantity++;
+        } else {
+            delete panier[productId];
+        }
+        localStorage.setItem("panier", JSON.stringify(panier));
+        // ); /* Supprime item du localStorage */
+        container.remove(); /* Supprime item du DOM */
+        location.reload(true);
+    });
 };
 
 //Validation de notre formulaire de Commande
@@ -148,38 +109,44 @@ const checkValidity = (input) => {
     })
 
     input.addEventListener('input', (e) => {
-        if (e.target.validity.valid) {
-            e.target.parentElement.classList.remove('erreur__formulaire')
-            return (cartInformation.contact = {
-              // Si toutes les inputs saisies sont valides, renvoie l'objet contact à cartInformation
-              lastName: nomclient.value,
-              firstName: prenomclient.value,
-              email: emailclient.value,
-              address: adresseclient.value,
-              city: villeclient.value,
-        })}
+            if (e.target.validity.valid) {
+                e.target.parentElement.classList.remove('erreur__formulaire')
+                return (cartInformation.contact = {
+                    // Si toutes les inputs saisies sont valides, renvoie l'objet contact à cartInformation
+                    lastName: nomclient.value,
+                    firstName: prenomclient.value,
+                    email: emailclient.value,
+                    address: adresseclient.value,
+                    city: villeclient.value,
+                })
+            }
         }
-  );
+    );
 
-Array.from(inputs).forEach(checkValidity);
-Array.from(textareas).forEach(checkValidity);
+    Array.from(inputs).forEach(checkValidity);
+    Array.from(textareas).forEach(checkValidity);
 
 // Envoie données à l'api
-post ("https://oc-p5-api.herokuapp.com/api/cameras/order").then((response)=>{
-  const btn = document.getElementById("envoiformulaire");
-btn.addEventListener("click", (e) => {
-  e.preventDefault();
-  let panier = JSON.parse(localStorage.getItem("panier"));
-  if (panier === null) {
-    console.log ("impossible d'envoyer le formulaire de commande")
-  }else {
-          postData(
-         "POST",
-         "https://oc-p5-api.herokuapp.com/api/cameras/order",
-          cartInformation
-    ); // Envoie données au serveur
-    window.location = `./confirmation-de-commande.html?id=${response.orderId}&price=${totalPrice}&user=${firstName.value}`; // Redirige vers la page de confirmation de commande
-  }})
+    post("https://oc-p5-api.herokuapp.com/api/cameras/order").then((response) => {
+        const btn = document.getElementById("envoiformulaire");
+        btn.addEventListener("click", (e) => {
+            e.preventDefault();
+            let panier = JSON.parse(localStorage.getItem("panier"));
+            if (panier === null) {
+                console.log("impossible d'envoyer le formulaire de commande")
+            } else {
+                postData(
+                    "POST",
+                    "https://oc-p5-api.herokuapp.com/api/cameras/order",
+                    cartInformation
+                ); // Envoie données au serveur
+                window.location = `./confirmation-de-commande.html?id=${response.orderId}&price=${totalPrice}&user=${firstName.value}`; // Redirige vers la page de confirmation de commande
+            }
+        })
+    })
+}
 
-
-});
+const panier = JSON.parse(localStorage.getItem('panier'))
+for(let product of Object.values(panier)){
+    renderCart(product.name, product.price, product.imageUrl, product.quantity)
+}
