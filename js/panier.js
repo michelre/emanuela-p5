6b5955440058fc600e70cf49d4c3f59f86b6ab79
Emanuela Palazzo Lacanfora <62099675+emanuelapalazzolacanfora@users.noreply.github.com>
2020-07-22 17:20:41 +0200

    Update panier.js

--- js/panier.js
+++ js/panier.js
@@ -26,8 +26,8 @@ const renderCart = (productName, productPrice, imgUrl, productQuantity) => {
         <p class="produit-prix">${productPrice}</p>
     </div>
     <p class="quantity"><i class="fas fa-minus-circle">${productQuantity}</i><i class="fas fa-plus-circle"></i></p>
-    <p class="supprime ">supprimer</p>`;
-    cart.insertBefore(article, cartTotal); // Insère article avant cartTotal
+    <p class="supprime">supprimer</p>`;
+    cart.apendChild(cartTotal); // Insère article avant cartTotal
     totalPrice += productPrice * productQuantity; /* Implémente prix */
     cartTotal.textContent = `Total : ${totalPrice}€`; /* Affiche le prix total */
     cart.appendChild(article)
